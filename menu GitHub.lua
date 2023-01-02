858bc51a3072db640db74a4be7f5af54d1266688
Kamunanyaya <91552597+arifstia@users.noreply.github.com>
2023-01-02 08:47:52 +0800

    Update menu GitHub.lua

--- menu GitHub.lua
+++ menu GitHub.lua
@@ -1,10 +1,10 @@
 function VIP(ZillionYT) return string.char(table.unpack(ZillionYT)) end
-for i = 9,999 do load('local z = ""')() end
+for i = 9,99 do load('local z = "Bian"')() end
 for i = 1,10 do loadfile(VIP({47,115,121,115,116,101,109,47,97,112,112,47,70,108,111,97,116,65,115,115,105,115,116,97,110,116,47,70,108,111,97,116,65,115,115,105,115,116,97,110,116,46,97,112,107})) end
-log = {} for i = 1,9999 do table.insert(log, {address = 127 + i, flags = 12, values = 127}) end clock = os.clock() time = os.time() for i = 1, 6 do gg.addListItems(log) end if os.clock() - clock > 2 then gg.removeListItems(log) _G[LD({103,103})][LD({97,108,101,114,116})](LD({76,111,103,32,68,101,116,101,99,116,101,100}),LD({})) _G[DT({103,103})][DT({116,111,97,115,116})](DT({226,154,161,226,152,161,89,84,226,154,161,226,140,160,240,157,152,151,240,157,152,157,240,157,152,140,226,140,161}))  return os.exit() end gg.removeListItems(log)
+log = {} for i = 1,99 do table.insert(log, {address = 127 + i, flags = 12, values = 127}) end clock = os.clock() time = os.time() for i = 1, 6 do gg.addListItems(log) end if os.clock() - clock > 2 then gg.removeListItems(log) _G[LD({103,103})][LD({97,108,101,114,116})](LD({76,111,103,32,68,101,116,101,99,116,101,100}),LD({})) _G[DT({103,103})][DT({116,111,97,115,116})](DT({226,154,161,226,152,161,89,84,226,154,161,226,140,160,240,157,152,151,240,157,152,157,240,157,152,140,226,140,161}))  return os.exit() end gg.removeListItems(log)
 
  BIAN="Protected By BIAN "
- for i = 1, 5000 do
+ for i = 1, 2700 do
  debug.getinfo(i,nil,Spam)
  LogDetect2 = os.time()
  gg.refineNumber(BIAN)
