875a626afe636bd16b65a9de9aa89abf940d3c73
Kamunanyaya <91552597+arifstia@users.noreply.github.com>
2023-02-06 06:26:23 +0800

    Create Live.lua

---
+++ Live.lua
@@ -0,0 +1,7 @@
+gg.alert(" script thief, not creative, 😝")
+gg.alert(" https://github.com/thatskymod/Sky-CotL-Scripts")
+gg.alert(" fuck you ")
+
+--Contact me on telegram if you really are a man
+--Bian#9853
+-- telegram you know lah 🧐🧐
